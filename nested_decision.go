b813b013c0fcc90b5e2db54cfbac2c30d48e0f78
irpanrain <irpanar97@gmail.com>
2021-08-22 19:03:37 +0700

    continue to learn go

--- nested_decision.go
+++ nested_decision.go
@@ -19,4 +19,27 @@ func main() {
 			fmt.Printf("Value v1 is %d and value v2 is %d\n", v1, v2)
 		}
 	}
+
+	// use of if..else..if ladder
+	// taking a local variable
+	var v int = 700
+
+	// checking the condition
+	if (v == 100) {
+
+		// if condition is true then display the following
+		fmt.Printf("Value of v is 100\n")
+	} else if(v == 200) {
+
+		fmt.Printf("Value of v is 200\n")
+
+	} else if(v == 300) {
+
+		fmt.Printf("Value of v is 300\n")
+
+	} else {
+
+		// if none of the condition is true
+		fmt.Printf("None of the values is matching\n") 
+	}
 }
